6ef6c1596e94c1eca072ec83c2b400ba97646cb0
ffelipeao <feliperj629@gmail.com>
2026-09-09 17:30:45 -0300

    identando arquivos.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
-const anoAtual = document.querySelector('#ano-atual');
-const secoes = document.querySelectorAll('main section[id]');
-const linksMenu = document.querySelectorAll('.menu__links a');
+const anoAtual = document.querySelector("#ano-atual");
+const secoes = document.querySelectorAll("main section[id]");
+const linksMenu = document.querySelectorAll(".menu__links a");
 
 anoAtual.textContent = new Date().getFullYear();
 
-const observador = new IntersectionObserver((entradas) => {
+const observador = new IntersectionObserver(
+  (entradas) => {
     entradas.forEach((entrada) => {
-        if (!entrada.isIntersecting) return;
+      if (!entrada.isIntersecting) return;
 
-        linksMenu.forEach((link) => {
-            const secaoAtual = link.getAttribute('href') === `#${entrada.target.id}`;
-            link.classList.toggle('ativo', secaoAtual);
-        });
+      linksMenu.forEach((link) => {
+        const secaoAtual =
+          link.getAttribute("href") === `#${entrada.target.id}`;
+        link.classList.toggle("ativo", secaoAtual);
+      });
     });
-}, { rootMargin: '-35% 0px -55%' });
+  },
+  { rootMargin: "-35% 0px -55%" },
+);
 
 secoes.forEach((secao) => observador.observe(secao));
